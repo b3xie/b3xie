d6a62fd555a6d017f944451f81c052c25588f40e
Bex <rebexie@b3x.gay>
2023-10-21 00:01:19 -0300

    initial commit

---
+++ cmd/web/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Iosevka Aile Iaso", "sans-serif"],
+        mono: ["Iosevka Curly Iaso", "monospace"],
+        serif: ["Iosevka Etoile Iaso", "serif"],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
